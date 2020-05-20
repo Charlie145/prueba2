6eb4f14167a7219c55acce3955ed5bfac6481e98
Charlie145 <32180540+Charlie145@users.noreply.github.com>
2020-05-19 21:06:33 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,27 @@
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+$('#myModal2').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+$('#myModal3').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+ $(document).ready(function(){
+        $('.myCarousel').carousel({
+            interval: 2000
+            pause:hover
+        });
+    });   
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+$('#myModal2').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#myModal3').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+$('#myModal4').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+$('#myModal5').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
